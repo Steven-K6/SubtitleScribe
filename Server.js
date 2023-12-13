548d06746e8d3b077d30c08e6b90878409e79e1c
Steven K <stevenk@stevens-mbp.mynetworksettings.com>
2023-12-12 22:59:23 -0500

    integrated Google Cloud Storage and finished the front-end design

--- Server.js
+++ Server.js
@@ -2,173 +2,233 @@ require('dotenv').config();
 /* loads configuration settings from an .env file. This is used to store sensitive
 information seperately from the code*/
 
-const express = require('express');
+const express = require('express'); 
 // imports express.js framework which is used for web applications
 
 const multer = require('multer');
 //imports the Multer middleware which handles file uploads
 
-const path = require('path');
-/* imports the Node.js module 'path' which allows you to work 
-with file and directory paths */
+const { Storage } = require('@google-cloud/storage');
+// the Google Cloud Storage module for Node.js
+
+const { SpeechClient } = require('@google-cloud/speech');
+// the Google Cloud Speech-to-Text module for Node.js
 
 const ffmpeg = require('fluent-ffmpeg');
 /* imports the ffmpeg libary which works with audio and video files */
+
+const fs = require('fs');
+// the Node.js filesystem module for interacting with the file system
 
 const app = express();
 /* Creates an instance of the Express.js application by invoking
 the express() function. The app 'object' is used to define routes, 
 and middleware */
+
 const port = 3000;
 /* Defines a constant 'port' and assigns it the value of 3000. 
 When you start the server is will listen on port 3000 and any incoming 
 requests will be directed to the application on this port */
 
-const storage = multer.diskStorage({
-/*assigns a variable named storage to an instance of Multers diskstorage 
-engine. diskstorage is used to specify how uploaded files should be stored
-on the server's disk */
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-        //defines the directory where the uploaded files will be saved
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-        //defines how the uploaded file should be named on the server
-    },
+const storageClient = new Storage({
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 });
-/*Uploaded videos will be saved to the 'uploads' directory with their
-orignal filenames*/
-
-const upload = multer({ storage: storage });
-/* configures Multer for handling file uploads. The storage object
-defines how uploaded files should be stored, which was previously
-explained */
-
-const { SpeechClient } = require('@google-cloud/speech');
-//imports SpeechClient class from the google-cloud/speach package
-// the SpeechClient class provides methods for interacting with the Speech to text API
+/* creates a new instance of the Storage class.
+The keyFilename is set to refer to an environment variable that should
+contain the path to the JSON file containing the Google Cloud service
+account key. */
 
 const speechClient = new SpeechClient({
-keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 });
 /* creates a new instance of the SpeechClient class.
 The keyFilename is set to refer to an environment variable that should
 contain the path to the JSON file containing the Google Cloud service
 account key. */
 
+const bucketName = 'subtitlescribebucket'; 
+const bucket = storageClient.bucket(bucketName);
+// sets the bucketName variable to the name of the Google Cloud storage bucket
+
+ const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+// Multer is set to use 'memoryStorage' which stores files in memory
+
 app.use(express.static('public'));
 /* serves static files from the 'public' directory. (html,css javascript)
 Allows web browsers to access these files directly */
 
-app.use('/video', express.static('uploads'));
-//serves uploaded videos from the 'uploads' directory
-
-app.use('/audio', express.static('audio'));
-//serves static files from the 'audio' directory
-
-app.post('/upload', upload.single('video'), (req, res) => {
-  /* defines a route that handles HTTP post requests.
-  upload.single('video') is a middleware provided by Multer, 
+app.post('/upload', upload.single('video'), async (req, res) => {
+    /* defines a route that handles HTTP post requests
+    upload.single('video') is a middleware provided by Multer, 
   it expects a single file upload with the name 'video' in the request */
-    
+  try {
     if (!req.file) {
-        res.status(400).send('No video file uploaded.');
-        return;
-        /* checks if there is a file upload in the request.
-        If no file was uploaded the message will be displayed */
+      res.status(400).send('No video file uploaded.');
+      return;
     }
+      /* checks if there is a file upload in the request.
+        If no file was uploaded the message will be displayed */
+    
 
-const inputVideoFilePath = `uploads/${req.file.filename}`;
-/* constructs the path to the uploaded video file. Path to the input */
+    const videoFilename = req.file.originalname;
+    // retrieves the originalname of the uploaded video file
 
-const outputAudioFilePath = `audio/${req.file.filename.replace(/\.[^/.]+$/, '')}.mp3`; // Output audio format is MP3
-/* constructs the path to the output audio file. Its a path to the mp3 
-audio file in the 'audio' directory */
+    const videoPath = `uploads/${videoFilename}`;
+    // creates a path for saving the video file in the 'uploads' directory
 
-ffmpeg() //uses `fluent-ffmpeg` library to perform audio extraction
-    .input(inputVideoFilePath) //specifies that the input video will be processed
-    .audioCodec('libmp3lame') // specifies the audio codec
-    .toFormat('mp3') // sets the output to mp3
-    .on('end', () => {
+    const audioFilename = `${videoFilename.replace(/\.[^/.]+$/, '')}.mp3`;
+    //replaces the the extension of the audio file with 'mp3'
+
+    const audioPath = `audio/${audioFilename}`;
+    // creates a path for saving the audio file in the 'audio' directory
+
+    const videoFile = bucket.file(videoPath);
+    await videoFile.save(req.file.buffer);
+    // saves the video file to Google Cloud Storage bucket
+
+    ffmpeg()
+      .input(videoFile.createReadStream()) //specifies the input for FFmpeg
+      .audioCodec('libmp3lame') // specifies the audio codec
+      .toFormat('mp3') // sets the output to mp3
+      .on('end', async () => {
         console.log('Audio extraction and conversion complete.');
-    })
-    .on('error', (err) => {
+        // extracts the audio from the video using FFmpeg
+
+        const audioFile = bucket.file(audioPath);
+        await audioFile.save(req.file.buffer, {
+        metadata: {
+            contentType: 'audio/mpeg',
+            // this saves the extracted audio to the Google Cloud Storage bucket
+        },
+    });
+
+        const publicURL = `https://storage.googleapis.com/${bucketName}/${videoPath}`;
+        res.send(publicURL);
+        // provides the public URL of the uploaded audio in Google Cloud Storage
+      })
+      .on('error', (err) => {
         console.error('Error extracting audio:', err);
-    })
-    //registers an event handler that is triggered when the process is complete
-
-    .save(outputAudioFilePath);
-//specifies the path where the output audio files should be saved
-
-    res.send(`/video/${req.file.filename}`);
+        res.status(500).send('Error extracting audio: ' + err.message);
+      })
+      .save(audioPath);
+      // saves the extracted audio in Google Cloud Storage
+  } catch (error) {
+    console.error('Error uploading and processing file:', error);
+    res.status(500).send('Error uploading and processing file: ' + error.message);
+  }
 });
 
 app.post('/transcribe-audio', upload.single('audio'), async (req, res) => {
-    //Checks if an audio file was uploaded
-    if (!req.file) {
-      res.status(400).send('No audio file uploaded.');
-      return;
-    }
-  
-    const inputFilePath = `uploads/${req.file.filename}`;
-    // Creates a path to the uploaded audio file
-  
-    const audioContent = await readFile(inputFilePath);
-    // reads the uploaded audio file
-  
-    const audio = {
-      content: audioContent.toString('base64'),
-    };
-    //prepares the audio content to be sent to the Speech-to-Text API
-  
-    const request = {
-      audio: audio,
-      config: {
-        encoding: 'MP3', 
-        sampleRateHertz: 16000, 
-        languageCode: 'en-US', 
-      },
-    };
-    // configures the transcription request
-  
+    /* creates a route at the path '/transcribe-audio'. it expects 
+    a single file with the name 'audio'. */
     try {
-      const [response] = await speechClient.recognize(request);
-      // sends the transcription request to the API
-      
-      const transcription = response.results[0].alternatives[0].transcript;
-    // logs the transcription response
+      if (!req.file) {
+        res.status(400).send('No audio file uploaded.');
+        return;
+      }
+      // checks if the audio file has been uploaded
+  
+      const audioPath = `audio/${req.file.originalname}`;
+      const audioFile = bucket.file(audioPath);
+      // creates the path and file object for storing audio files in Google Cloud Storage
+  
+      const writeStream = audioFile.createWriteStream();
+      writeStream.end(req.file.buffer);
+      await new Promise((resolve, reject) => {
+        writeStream.on('finish', resolve);
+        writeStream.on('error', reject);
+      });
+      // saves the audio file in Google Cloud Storage
+  
+      const audioDurationInSeconds = await getAudioDuration(audioFile);
+      // calls the 'getAudioDuration' function to get the duration of the uploaded audio file
+  
+      let transcription;
+      if (audioDurationInSeconds > 60) {
+        const storageUri = `gs://${bucketName}/${audioPath}`;
+        const longRunningRequest = {
+          audio: {
+            uri: storageUri,
+          },
+          config: {
+            encoding: 'MP3',
+            sampleRateHertz: 16000,
+            languageCode: 'en-US',
+          },
+        };
 
+        const [operation] = await speechClient.longRunningRecognize(longRunningRequest);
+        const [response] = await operation.promise();
+        transcription = response.results.map(result => result.alternatives[0].transcript).join(' ... ');
+         // use LongRunningRecognize for audio longer than 1 minute
+
+      } else {
+        const audio = {
+          content: req.file.buffer.toString('base64'),
+        };
+  
+        const request = {
+          audio: audio,
+          config: {
+            encoding: 'MP3',
+            sampleRateHertz: 16000,
+            languageCode: 'en-US',
+          },
+        };
+  
+        const [response] = await speechClient.recognize(request);
+        transcription = response.results.map(result => result.alternatives[0].transcript).join(' ... ');
+      }
+      /* use recognize for audio shorter than 1 minute.
+       Performs the transcription using the Google Cloud Speech-to-Text
+       API and formats the transcription with '...' between segements. */
+      
       res.status(200).json({ transcription });
     } catch (error) {
       console.error('Error transcribing audio:', error);
       res.status(500).json('Error transcribing audio.');
     }
   });
-  
-  function readFile(filePath) {
-    const fs = require('fs');
+
+  async function getAudioDuration(audioFile) {
+    // declares an asynchronous function names 'getAudioDuration'
+
     return new Promise((resolve, reject) => {
-      fs.readFile(filePath, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
+      const tmpFilePath = 'temp_audio.mp3';
+      // creates a temporary file to store the audio buffer
+  
+      audioFile.createReadStream()
+        .pipe(fs.createWriteStream(tmpFilePath))
+        // downloads the audio file from Google Cloud Storage
+
+        .on('finish', () => {
+          ffmpeg.ffprobe(tmpFilePath, (err, metadata) => {
+            if (err) {
+              reject(err);
+            } else {
+            /* uses 'ffprobe' from the 'fluent-ffmpeg' library to get the 
+            duration of the audio file */
+              resolve(metadata.format.duration);
+              // resolves with the duration in seconds
+            }
+  
+            fs.unlinkSync(tmpFilePath);
+            // removes the temporary file
+          });
+        })
+        .on('error', reject);
     });
   }
-// Function to read the file content asynchronously
-
+  
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/upload.html');
-}); 
-/*when the user selects the root URL of the application, the upload.html
+  res.sendFile(__dirname + '/public/Home.html');
+});
+/*when the user selects the root URL of the application, the Home.html
 file will be sent as a response. Shows the HTML file in the browser*/
 
-
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 /*starts the express server on port 3000 and logs a message
 indicating that the server is running */
--- public/script.js
+++ public/script.js
@@ -1,78 +1,78 @@
 document.addEventListener('DOMContentLoaded', () => {
     /* makes sure that the code doesnt run before the document is ready.
     DOMContentLoaded allows you to modify the HTML elements */
-    
-        const videoElement = document.getElementById('uploadedVideo');
-        const uploadForm = document.getElementById('uploadForm');
-        const uploadButton = document.getElementById('uploadButton');
-        const fileInput = document.querySelector('input[type="file"]');
-        // References to HTML elements by their ID
-    
-        uploadButton.addEventListener('click', async () => {
-        // adds a click event listener to the upload button
-    
-            const formData = new FormData(uploadForm);
-            /* object formData is created to send the uploaded file
-            via AJAX to the server */
-    
-            try {
-                const response = await fetch('/upload', {
-                    method: 'POST',
-                    body: formData,
-                });
-                /* sends a POST request to the server at the /upload
-                endpoint using fetch. It sends the uploaded file in the 
-                formData object. Using await makes sure that the response 
-                is recieved before the function executes */
-    
-                if (response.ok) {
-                    const videoURL = await response.text();
-                    /* extracts the url of the uploaded video from the 
-                    server response */
-                    videoElement.src = videoURL;
-                    /* sets the src attribute of the HTML videoElement 
-                    to the videos URL, which will load and display the video */
-                    videoElement.style.display = 'block';
-                    /* if the file upload request is successful. The
-                    uploaded video will be displayed*/
-                } else {
-                    console.error('Upload failed:', response.statusText);
-                    /* if there is an error. It will log the error
-                    message to the console */
-                }
-            } catch (error) {
-                console.error('Error:', error);
-                /* catches any unexpected errors like network issues and is 
-                logged to the console */
-            }
-        });
-    });
-    
-    async function transcribe() {
-        const audioInput = document.getElementById('audioInput');
-        const transcriptionDiv = document.getElementById('transcription');
-      //references HTML elements by their IDs
 
-        const audioFile = audioInput.files[0];
-        if (!audioFile) {
-          alert('Please select an audio file.');
-          return;
-        }
-        //checks if there is a selected audio file in the file input element
-      
-        const formData = new FormData();
-        formData.append('audio', audioFile);
-      
-        try {
-          const response = await fetch('/transcribe-audio', {
+    const videoElement = document.getElementById('uploadedVideo');
+    const uploadForm = document.getElementById('uploadForm');
+    const uploadButton = document.getElementById('uploadButton');
+    const fileInput = document.querySelector('input[type="file"]');
+    // References to HTML elements by their ID
+
+    uploadButton.addEventListener('click', async () => {
+        // adds a click event listener to the upload button
+        await uploadVideo();
+    });
+});
+
+async function transcribe() {
+    // declares an asychronous function named 'transcribe'
+
+    const audioInput = document.getElementById('audioInput');
+    const transcriptionDiv = document.getElementById('transcription');
+    // references HTML elements with their IDs
+
+    const audioFile = audioInput.files[0];
+    if (!audioFile) {
+        alert('Please select an audio file.');
+        return;
+    }
+    // retrieves the selected audio file from the file input 'audioInput'
+
+    const formData = new FormData();
+    formData.append('audio', audioFile);
+
+    try {
+        const response = await fetch('/transcribe-audio', {
             method: 'POST',
             body: formData,
-          });
-      
+        });
+        // sends a POST request to the '/transcribe-audio' endpoint
+
         const { transcription } = await response.json();
         transcriptionDiv.textContent = transcription;
-} catch (error) {
-    console.error('Unexpected error:', error);
-    alert('Unexpected error during transcription.');
+        // the transcription result is taken from the JSON file and displayed in the transcriptionDiv element
+
+    } catch (error) {
+        console.error('Unexpected error:', error);
+        alert('Unexpected error during transcription.');
+    }
+}
+
+async function uploadVideo() {
+    // declares an asychronous function named 'uploadVideo()'
+
+    const videoElement = document.getElementById('uploadedVideo');
+    const uploadForm = document.getElementById('uploadForm');
+     //references HTML elements by their IDs
+    
+    const formData = new FormData(uploadForm);
+
+    try {
+        const response = await fetch('/upload', {
+            method: 'POST',
+            body: formData,
+        });
+        // sends a POST request to the '/upload' endpoint
+
+        if (response.ok) {
+            const videoURL = await response.text();
+            videoElement.src = videoURL;
+            videoElement.style.display = 'block';
+        } else {
+            console.error('Upload failed:', response.statusText);
+            // displays the video element if successful
+        }
+    } catch (error) {
+        console.error('Error:', error);
     }
 }
